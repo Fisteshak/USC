f11fc429dcf07d99261517e3266b6460a1f3c31c
Fisteshak <antonlazarev2005@gmail.com>
2022-08-30 16:54:03 +0300

    simple echo server with bufferevent

--- server/UServer.cpp
+++ server/UServer.cpp
@@ -1,10 +1,34 @@
 #include "UServer.h"
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include "windows.h"
+
+#include <iostream>
+#include <string>
+
+#include "event2/event.h"
+#include "event2/listener.h"
+
+
+
 UServer::UServer()
 {
+
 
 }
 
 UServer::~UServer()
 {
+}
 
+bool initWinsock()
+{
+    WSADATA wsaData;
+    WORD ver = MAKEWORD(2, 2); //версия Winsock
+    if (WSAStartup(ver, &wsaData) != 0)
+    {
+        std::cout << "Error: can't initialize winsock!" << WSAGetLastError();        
+        return false;
+    }
+    return true;
 }
--- server/UServer.h
+++ server/UServer.h
@@ -1,17 +1,15 @@
 #pragma once
+#include <string>
 class UServer
 {
 private:
     /* data */
 public:
-    UServer(/* args */);
+    UServer();
     ~UServer();
+
 };
 
-UServer::UServer(/* args */)
-{
-}
+bool initWinsock();  //Инициализирует сетевой интерфейс для сокетов.
+                     //Возвращает true в случае успеха, false в случае неудачи.
 
-UServer::~UServer()
-{
-}
