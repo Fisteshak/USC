d2924128ab69625d7d969a44639822cea1e7096c
Fisteshak <antonlazarev2005@gmail.com>
2022-12-13 20:00:07 +0300

    Вывод списка процессов

--- BigBrother'sEye/BBE-client.cpp
+++ BigBrother'sEye/BBE-client.cpp
@@ -1,64 +1,17 @@
-#include "UClient.h"
 #include <iostream>
-#include <string>
-
-
-void data_handler(UClient::DataBuffer& data)
-{
-    // for (int i = 0; data[i] != '\0'; i++) {
-    //     std::cout << data[i];
-    // }
-    for (int i = 0; i < data.size(); i++) {
-        std::cout << data[i];
-    }
-    std::cout << std::endl;
-}
-
-void conn_handler()
-{
-    std::cout << "[client] Succesfully connected to server" << std::endl;
-}
-
-void disconn_handler()
-{
-    std::cout << "[client] Disconnected from server" << std::endl;
-}
-
+#include "Windows.h"
+#include "WtsApi32.h"
 int main()
 {
-    UClient client;
-
-    client.setDataHandler(data_handler);
-    client.setDisconnHandler(disconn_handler);
-    client.setConnHandler(conn_handler);
-
-    std::cout << "Write your name: " << std::endl;
-
-    std::string name;
-    std::getline(std::cin, name, '\n');
-
-    client.connectTo("127.0.0.1", 9554);
-
-    client.sendPacket(name);
-
-    if (client.getStatus() != UClient::status::connected) {
-        std::cout << "Failed to connect to a server\n";
-        return 0;
-    }
-
-    UClient::DataBufferStr data;
-
-    while (true) {
-
-
-        std::getline(std::cin, data, '\n');
-
-        if (data == ":stop" || client.getStatus() != UClient::status::connected) {
-            break;
+    WTS_PROCESS_INFO* pWPIs = NULL;
+    DWORD dwProcCount = 0;
+    if (WTSEnumerateProcesses(WTS_CURRENT_SERVER_HANDLE, NULL, 1, &pWPIs, &dwProcCount)) {
+        for (DWORD i = 0; i < dwProcCount; i++) {
+            std::cout << pWPIs[i].pProcessName << std::endl;
         }
-        client.sendPacket(data);
     }
 
-
-    client.disconnect();
+    if (pWPIs) {
+        WTSFreeMemory(pWPIs);
+    }
 }
